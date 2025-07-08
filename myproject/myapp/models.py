23c061d2e9b14a36fe60755fd808d2aacf56d8b8
christina <158211764+christinac09@users.noreply.github.com>
2025-07-08 12:57:29 -0400

    migrate

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,26 +1,59 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
+import uuid
 
-# Create your models here.
-class User(AbstractUser):
-    
-    
-    class Types(models.TextChoices):
-        ADMIN = "ADMIN", "Admin"
-        USER = "USER", "User"
+class Role(models.Model):
+    USER = 1
+    ADMIN = 2
+    ROLE_CHOICES = ((USER, "user"), (ADMIN, 'admin'))
 
-    email = models.EmailField(null=False, unique=True)
-    username = models.CharField(max_length=255, null=False, unique=True)
-    password = models.CharField(max_length=255, null=False)
-    type = models.CharField(max_length=20, choices = Types.choices, default=Types.USER)
+    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
 
     def __str__(self):
-        return self.username
+        return self.get_id_display()
+
+
+class User(AbstractUser):
+    role = models.ForeignKey(Role, null=False)
+    email = models.EmailField(null=False, unique=True)
+    username = models.CharField(max_length=255, null=False, unique=True)
+    #password = models.CharField(max_length=255, null=False)  included in abstractuser(?)
     
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username',]
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+
+class Type(models.Model):
+    MULTIPLE_CHOICE = 1
+    TRUE_OR_FALSE = 2
+    DROPDOWN = 3   #fill in the blank with dropdown
+    NUMERICAL = 4
+    TYPE_CHOICES = ((MULTIPLE_CHOICE, 'multiple_choice'), (TRUE_OR_FALSE, 'true_or_false'), (DROPDOWN, 'dropdown'), (NUMERICAL, 'numerical'))
+
+    id = models.PositiveSmallIntegerField(choices=TYPE_CHOICES, primary_key=True)
+    
+    def __str__(self):
+        return self.get_id_display()
+    
+class Quiz(models.Model):
+    uuid_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255, null=False)
+    types = models.ManyToManyField(Type, null=False)
+    creator = models.ForeignKey(User, null=False)
+    
+    def __str__(self):
+        return self.uuid_id
 
 class Question(models.Model):
-    question = models.TextField(null=False)
-    answer = models.TextField(null=False)
+    uuid_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.CharField(max_length=255, null=False)
+    answer = ArrayField(models.CharField(max_length=255), null=False, blank=False, default=list)
+    incorrect = ArrayField(models.CharField(max_length=255), blank=True, default=list)
+    quiz = models.ForeignKey(Quiz, null=False)
 
-class Quiz(models.Model):
-    questions = models.
+    def __str__(self):
+        return self.uuid_id
